4a710229e5e04bfd0b247bebba308a95f3597f62
NatanielSilva <nataniel.silva@techne.com.br>
2025-04-09 14:53:52 -0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,21 @@
 node default {
-  include confapps
-}
+  # Garante que o diretório existe
+  file { '/opt/techne/lyceum/tomcat/conf/Catalina/localhost':
+    ensure => directory,
+    owner  => 'tomcat',
+    group  => 'tomcat',
+    mode   => '0755',
+  }
 
+  # Copia cada XML para o Tomcat
+  file { '/opt/techne/lyceum/tomcat/conf/Catalina/localhost/consulta-documentos.xml':
+    ensure => file,
+    source => 'puppet:///modules/lyceum_xmls/consulta-documentos.xml',  # Caminho no Puppet Master
+    owner  => 'tomcat',
+    group  => 'tomcat',
+    mode   => '0644',
+  }
+
+  # Repita para os outros 3 arquivos (dashboard.xml, diploma-digital.xml, webservices.xml)
+  # ...
+}
